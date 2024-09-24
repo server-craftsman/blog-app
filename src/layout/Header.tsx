493362be0e42a3432d095a4816c838e8e955858b
server-craftsman <huyit2003@gmail.com>
2024-09-24 19:59:44 +0700

    add responsive more devices

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
+
 const Header = () => {
   return (
-    <header className="bg-white shadow">
-      <div className="container mx-auto px-4 py-2 flex justify-between items-center">
-        <div className="flex items-center">
+    <header className="bg-white shadow-lg">
+      <div className="container mx-auto px-6 py-4 flex flex-col sm:flex-row justify-between items-center">
+        <div className="flex items-center mb-2 sm:mb-0">
           <Link to="/" className="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 mr-2 text-blue-600">
               <path fillRule="evenodd" d="M4.125 3C3.089 3 2.25 3.84 2.25 4.875V18a3 3 0 0 0 3 3h15a3 3 0 0 1-3-3V4.875C17.25 3.839 16.41 3 15.375 3H4.125ZM12 9.75a.75.75 0 0 0 0 1.5h1.5a.75.75 0 0 0 0-1.5H12Zm-.75-2.25a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5H12a.75.75 0 0 1-.75-.75ZM6 12.75a.75.75 0 0 0 0 1.5h7.5a.75.75 0 0 0 0-1.5H6Zm-.75 3.75a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5H6a.75.75 0 0 1-.75-.75ZM6 6.75a.75.75 0 0 0-.75.75v3c0 .414.336.75.75.75h3a.75.75 0 0 0 .75-.75v-3A.75.75 0 0 0 9 6.75H6Z" clipRule="evenodd" />
@@ -14,9 +15,9 @@ const Header = () => {
           </Link>
         </div>
         <SearchBar onSearch={() => {}} placeholder="Search blogs" />
-        <nav className="flex items-center space-x-1">
-          <Link to="/" className="header-link">Home</Link>
-          <div className="relative group">
+        <nav className="flex flex-col sm:flex-row items-center space-x-0 sm:space-x-2">
+          <Link to="/" className="header-link mb-1 sm:mb-0">Home</Link>
+          <div className="relative group mb-1 sm:mb-0">
             <button className="header-link flex items-center">
               Products
               <svg className="ml-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -28,7 +29,7 @@ const Header = () => {
               <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Product 2</a>
             </div>
           </div>
-          <div className="relative group">
+          <div className="relative group mb-1 sm:mb-0">
             <button className="header-link flex items-center">
               Resources
               <svg className="ml-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -40,25 +41,17 @@ const Header = () => {
               <a href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100">Resource 2</a>
             </div>
           </div>
-          <a href="#" className="header-link">Pricing</a>
-          <Link to="/dashboard" className="header-link">
+          <a href="#" className="header-link mb-1 sm:mb-0">Pricing</a>
+          <Link to="/dashboard" className="header-link mb-1 sm:mb-0">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-1">
               <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
             </svg>
             Write
           </Link>
-          {/* <div className="relative">
-            <button className="text-gray-700 hover:text-gray-900">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-              </svg>
-            </button>
-            <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">New</span>
-          </div> */}
         </nav>
-        <div className="flex space-x-4 items-center">
-          <Link to="/login" className="text-gray-700 hover:text-gray-900">Log in</Link>
-          <Link to="/register" className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800">Sign up</Link>
+        <div className="flex flex-col sm:flex-row space-x-0 sm:space-x-4 items-center">
+          <Link to="/login" className="text-gray-700 hover:text-gray-900 mb-1 sm:mb-0">Log in</Link>
+          <Link to="/register" className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800 transition duration-200">Sign up</Link>
         </div>
       </div>
     </header>
